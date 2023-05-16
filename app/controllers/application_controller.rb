2f0d557d94ab16b6251ddeb7da41798f7c32bea5
Kanatsanan Janpakdee <kanatsanan.j1998@gmail.com>
2023-05-17 02:31:13 +0700

    feat: add protect_from_forgery

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :null_session
   add_flash_types :success, :error
 
   def paginate(records)
